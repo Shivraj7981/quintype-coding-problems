2913fded771e579d7a7b954bfb77659e2d397bdf
Shivraj Waghmare <Shivraj.Waghmare@searshc.com>
2018-11-04 19:27:13 +0530

    Added a function to find range btw given number ie immidiate less and immidiate greater number

--- app/client/app.js
+++ app/client/app.js
@@ -8,6 +8,30 @@ global.startApp = function(container) {
 
   app.controller('diamondGame', function($scope, $http, $window) {
 
+    function findRange(currentIndex) {
+      var rightGreaterNumber;
+      var leftSmallerNumber;
+
+      // Finding immidiate greater no than currentIndex
+      for (var i = 0; i < $scope.diamonds.length; i++) {
+        if ($scope.diamonds[i] > currentIndex) {
+            rightGreaterNumber = $scope.diamonds[i];
+            break;
+        }
+      }
+      // Finding immidiate Smaller no than currentIndex
+      for (var i = $scope.diamonds.length; i >= 0; i--) {
+        if ($scope.diamonds[i] < currentIndex) {
+            leftSmallerNumber = $scope.diamonds[i];
+            break;
+        }
+      }
+      return {
+        left: leftSmallerNumber,
+        right: rightGreaterNumber
+      };
+    }
+
     // getting array of random diamond numbers. in the range of 1 to 64
     $scope.getRandomDiomondsNumber = function() {
       var count = 8;
@@ -28,7 +52,14 @@ global.startApp = function(container) {
     // getting array of random diamond numbers.
     $scope.diamonds = $scope.getRandomDiomondsNumber();
 
-
+    //Checking for the saved status of game if any
+    if (localStorage.getItem('gameItemCollection') != "null") {
+          $scope.gameItemCollection = JSON.parse(localStorage.getItem('gameItemCollection'));
+          $scope.diamonds = JSON.parse(localStorage.getItem('diamonds'))
+      } else {
+          $scope.gameItemCollection = new Array(64);
+      }
+    console.log($scope.diamonds)
 
     //break line condtion
     $scope.breakLine = function(index) {
