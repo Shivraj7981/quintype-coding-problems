81b6bb37dc6a3e15955f3aae99c79e44a3da29cd
Shivraj Waghmare <Shivraj.Waghmare@searshc.com>
2018-11-04 18:48:11 +0530

    added function to find random 8 diamond numbers

--- app/client/app.js
+++ app/client/app.js
@@ -7,6 +7,29 @@ global.startApp = function(container) {
   var app = angular.module('GameApp', []);
 
   app.controller('diamondGame', function($scope, $http, $window) {
+
+    // getting array of random diamond numbers. in the range of 1 to 64
+    $scope.getRandomDiomondsNumber = function() {
+      var count = 8;
+      var diamond = [];
+      var min = 1;
+      var max = 64;
+      while (count > 0) {
+        if (diamond.indexOf(parseInt(Math.random() * (+max - +min) + +min)) == -1) {
+            diamond.push(parseInt(Math.random() * (+max - +min) + +min));
+            count--;
+          }
+      }
+      return diamond.sort(function(a, b) {
+        return a - b
+      });
+    }
+
+    // getting array of random diamond numbers.
+    $scope.diamonds = $scope.getRandomDiomondsNumber();
+
+
+
     //break line condtion
     $scope.breakLine = function(index) {
         if ((index + 1) % 8 == 1 && index > 0) {
