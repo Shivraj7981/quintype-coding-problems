8fdac1f68c324b88ac941b33b5972dba129d3c3c
Shivraj Waghmare <Shivraj.Waghmare@searshc.com>
2018-11-04 15:14:46 +0530

    Added angularjs files

--- app/client/app.js
+++ app/client/app.js
@@ -1,4 +1,13 @@
 global.startApp = function(container) {
   console.log("Here is the container:", container);
-  
+  // Added qnagular js
+  var angular= require("angular");
+
+  var app = angular.module('GameApp', []);
+  app.controller('diamondGame', function($scope) {
+
+  });
 }
+
+
+
